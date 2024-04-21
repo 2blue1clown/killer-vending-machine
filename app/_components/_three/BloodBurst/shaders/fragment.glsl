1d790489b3b32cd3b0331d4db5b57e86ab63d473
bb-33 <jonolj56@gmail.com>
2024-04-21 16:39:14 +0100

    use step function to make particles disappear after a certain cutoff

--- app/_components/_three/BloodBurst/shaders/fragment.glsl
+++ app/_components/_three/BloodBurst/shaders/fragment.glsl
@@ -1,6 +1,8 @@
 uniform float uTime;
 varying vec4 vCalcPos;
 
-void main(){
-                gl_FragColor = vec4(1.0,0.0, 0.0, 1.0);
+void main(){  
+    float cutoff = 1.0;
+    float time = step(cutoff,uTime)* uTime - cutoff;
+                gl_FragColor = vec4(1.0,0.0, 0.0, 1.0-time);
             }
